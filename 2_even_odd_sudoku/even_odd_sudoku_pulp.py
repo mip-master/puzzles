b4308b032be2656d04db775bbfac4be1a2cc26b2
Luiz Henrique Suzana <luizhenriquesuzana02@gmail.com>
2021-07-07 09:52:02 -0300

    Implemented Aster's suggestions

--- 2_even_odd_sudoku/even_odd_sudoku_pulp.py
+++ 2_even_odd_sudoku/even_odd_sudoku_pulp.py
@@ -10,11 +10,11 @@ import pulp
 
 # region Input Data
 # Number of rows and columns
-Index = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+I = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 # Even cells
-EC = [(2, 1), (3, 2), (3, 5), (2, 6), (2, 7), (2, 8), (3, 8), (4, 8), (5, 7), (8, 7), (8, 9)]
+EC = {(2, 1), (3, 2), (3, 5), (2, 6), (2, 7), (2, 8), (3, 8), (4, 8), (5, 7), (8, 7), (8, 9)}
 # Odd cells
-OC = [(1, 2), (2, 3), (4, 4), (5, 3), (6, 2), (6, 6), (7, 2), (8, 2), (8, 3), (8, 4), (7, 5), (7, 8), (9, 8)]
+OC = {(1, 2), (2, 3), (4, 4), (5, 3), (6, 2), (6, 6), (7, 2), (8, 2), (8, 3), (8, 4), (7, 5), (7, 8), (9, 8)}
 # Given digits
 GD = {(1, 6): 4, (1, 7): 6, (1, 9): 9, (2, 5): 5, (3, 4): 1, (3, 9): 7, (4, 3): 4, (4, 9): 8, (5, 2): 2, (5, 8): 9,
       (6, 1): 1, (6, 7): 3, (7, 1): 9, (7, 6): 8, (8, 5): 6, (9, 1): 8, (9, 3): 5, (9, 4): 7}
@@ -30,7 +30,7 @@ BR = {1: [(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3)
       9: [(7, 7), (7, 8), (7, 9), (8, 7), (8, 8), (8, 9), (9, 7), (9, 8), (9, 9)]
       }
 # keys for decision variables x
-keys = [(i, j, k) for i in Index for j in Index for k in Index]
+keys = [(i, j, k) for i in I for j in I for k in I]
 # endregion
 
 # region Define the model
@@ -41,23 +41,23 @@ x = pulp.LpVariable.dicts(indexs=keys, cat=pulp.LpBinary, name='x')
 
 # Add constraints
 # Each cell must have exactly one digit
-for i in Index:
-    for j in Index:
-        mdl.addConstraint(pulp.lpSum(x[(i, j, k)] for k in Index) == 1, name=f'One-digit-per-cell-{(i,j)}')
+for i in I:
+    for j in I:
+        mdl.addConstraint(pulp.lpSum(x[(i, j, k)] for k in I) == 1, name=f'One-digit-per-cell-{(i,j)}')
 
 # Digits can't repeat in each row
-for i in Index:
-    for k in Index:
-        mdl.addConstraint(pulp.lpSum(x[i, j, k] for j in Index) == 1, name=f'No-repeat-row-{i}-value-{k}')
+for i in I:
+    for k in I:
+        mdl.addConstraint(pulp.lpSum(x[i, j, k] for j in I) == 1, name=f'No-repeat-row-{i}-value-{k}')
 
 # Digits can't repeat in each column
-for j in Index:
-    for k in Index:
-        mdl.addConstraint(pulp.lpSum(x[i, j, k] for i in Index) == 1, name=f'No-repeat-col-{j}-value-{k}')
+for j in I:
+    for k in I:
+        mdl.addConstraint(pulp.lpSum(x[i, j, k] for i in I) == 1, name=f'No-repeat-col-{j}-value-{k}')
 
 # Digits can't repeat in each bold region
 for region in BR:
-    for k in Index:
+    for k in I:
         mdl.addConstraint(pulp.lpSum(x[i, j, k] for (i, j) in BR[region]) == 1, name=f'No-repeat-region-{region}-value-'
                                                                                      f'{k}')
 
@@ -68,11 +68,11 @@ for i, j in GD:
 
 # Some cells must have an even digit
 for i, j in EC:
-    mdl.addConstraint(pulp.lpSum(x[i, j, k] for k in Index if k % 2 == 0) == 1, name=f'Even-digit-cell-{(i, j)}')
+    mdl.addConstraint(pulp.lpSum(x[i, j, k] for k in I if k % 2 == 0) == 1, name=f'Even-digit-cell-{(i, j)}')
 
 # Some cells must have an odd digit
 for i, j in OC:
-    mdl.addConstraint(pulp.lpSum(x[i, j, k] for k in Index if k % 2 == 1) == 1, name=f'Odd-digit-cell-{(i, j)}')
+    mdl.addConstraint(pulp.lpSum(x[i, j, k] for k in I if k % 2 == 1) == 1, name=f'Odd-digit-cell-{(i, j)}')
 
 # Set Objective function
 mdl.setObjective(x[1, 1, 1])  # we could choose any other non-constant function
@@ -86,6 +86,6 @@ mdl.solve()
 # print(f'x = {x_sol}')
 
 # Retrieve the solution (second option)
-x_sol2 = {(i, j): round(sum(k * x[i, j, k].value() for k in Index)) for i in Index for j in Index}
+x_sol2 = {(i, j): round(sum(k * x[i, j, k].value() for k in I)) for i in I for j in I}
 print(f'x = {x_sol2}')
 # endregion
