69ee97f53187f00b4e83472a141e8e7290c661a3
Aster Santana <50426897+astersantana@users.noreply.github.com>
2021-08-20 17:03:44 -0300

    Made some minor adjustments

--- 6_nonogram_aster/nonogram_scip.py
+++ 6_nonogram_aster/nonogram_scip.py
@@ -7,35 +7,33 @@ Created by Aster Santana and Éder Pinheiro (Aug, 2021), MipMaster.org.
 """
 
 from pyscipopt import Model, quicksum
-from collections import OrderedDict
 
 # region Input Data
 # row strings
-# using ordered dictionaries because the order of strings in each row/column matters in the implementation
 RS = {
-  1: OrderedDict({1: 4, 2: 1, 3: 2}),
-  2: OrderedDict({1: 3, 2: 1}),
-  3: OrderedDict({1: 1, 2: 3, 3: 2}),
-  4: OrderedDict({1: 1, 2: 1, 3: 1, 4: 2}),
-  5: OrderedDict({1: 1, 2: 1, 3: 1, 4: 1}),
-  6: OrderedDict({1: 1, 2: 1, 3: 1, 4: 1}),
-  7: OrderedDict({1: 1, 2: 1, 3: 1, 4: 4}),
-  8: OrderedDict({1: 1, 2: 3}),
-  9: OrderedDict({1: 1, 2: 4, 3: 2}),
-  10: OrderedDict({1: 2, 2: 2})
+  1: {1: 4, 2: 1, 3: 2},
+  2: {1: 3, 2: 1},
+  3: {1: 1, 2: 3, 3: 2},
+  4: {1: 1, 2: 1, 3: 1, 4: 2},
+  5: {1: 1, 2: 1, 3: 1, 4: 1},
+  6: {1: 1, 2: 1, 3: 1, 4: 1},
+  7: {1: 1, 2: 1, 3: 1, 4: 4},
+  8: {1: 1, 2: 3},
+  9: {1: 1, 2: 4, 3: 2},
+  10: {1: 2, 2: 2}
   }
 # column strings
 CS = {
-  1: OrderedDict({1: 1, 2: 1, 3: 3}),
-  2: OrderedDict({1: 1, 2: 1, 3: 1}),
-  3: OrderedDict({1: 1, 2: 1, 3: 1}),
-  4: OrderedDict({1: 1, 2: 2, 3: 1}),
-  5: OrderedDict({1: 3, 2: 3}),
-  6: OrderedDict({1: 3, 2: 2, 3: 3}),
-  7: OrderedDict({1: 3, 2: 4}),
-  8: OrderedDict({1: 1, 2: 1}),
-  9: OrderedDict({1: 1, 2: 2, 3: 1, 4: 2}),
-  10: OrderedDict({1: 6, 2: 2})
+  1: {1: 1, 2: 1, 3: 3},
+  2: {1: 1, 2: 1, 3: 1},
+  3: {1: 1, 2: 1, 3: 1},
+  4: {1: 1, 2: 2, 3: 1},
+  5: {1: 3, 2: 3},
+  6: {1: 3, 2: 2, 3: 3},
+  7: {1: 3, 2: 4},
+  8: {1: 1, 2: 1},
+  9: {1: 1, 2: 2, 3: 1, 4: 2},
+  10: {1: 6, 2: 2}
   }
 # rows
 I = list(RS.keys())
@@ -43,8 +41,8 @@ I = list(RS.keys())
 J = list(CS.keys())
 # keys for decision variables
 x_keys = [(i, j) for i in I for j in J]
-y_keys = [(i, j, k) for i in I for j in J for k in RS[i].keys()]
-z_keys = [(i, j, k) for i in I for j in J for k in CS[j].keys()]
+y_keys = [(i, j, k) for i in I for j in J for k in RS[i]]
+z_keys = [(i, j, k) for i in I for j in J for k in CS[j]]
 # endregion
 
 # region Define the model
@@ -60,36 +58,37 @@ for key in z_keys:
     z[key] = mdl.addVar(vtype='B', name=f'z_{key}')
 
 # add constraints
+# OBS: We could have combined the loops below to gain efficiency, but we kept them separated for clarity.
 # each row string begins in exactly one column
 for i in I:
-    for k in RS[i].keys():
+    for k in RS[i]:
         mdl.addCons(quicksum(y[i, j, k] for j in J) == 1, name=f'str_row{i}_{k}')
 # each column string begins in exactly one row
 for j in J:
-    for k in CS[j].keys():
+    for k in CS[j]:
         mdl.addCons(quicksum(z[i, j, k] for i in I) == 1, name=f'str_col{j}_{k}')
 # row strings length
 for i in I:
     for j in J:
-        for k in RS[i].keys():
+        for k in RS[i]:
             for t in range(RS[i][k]):
                 mdl.addCons(y[i, j, k] <= x.get((i, j+t), 0), name=f'row_len_{i}_{j}_{k}_{t}')
 # column strings length
 for i in I:
     for j in J:
-        for k in CS[j].keys():
+        for k in CS[j]:
             for t in range(CS[j][k]):
                 mdl.addCons(z[i, j, k] <= x.get((i+t, j), 0), name=f'col_len_{i}_{j}_{k}_{t}')
 # row strings disjunction and precedence
 for i in I:
     for j in J:
-        for k in RS[i].keys():
+        for k in RS[i]:
             for jp in range(1, j + RS[i][k]+1):  # the +1 ensures disjunction, i.e., an empty cell between strings
                 mdl.addCons(y.get((i, jp, k+1), 0) <= 1 - y[i, j, k], name=f'row_pre_{i}_{j}_{k}_{jp}')
-# # column strings disjunction and precedence
+# column strings disjunction and precedence
 for i in I:
     for j in J:
-        for k in CS[j].keys():
+        for k in CS[j]:
             for ip in range(1, i + CS[j][k]+1):  # the +1 ensures disjunction, i.e., an empty cell between strings
                 mdl.addCons(z.get((ip, j, k+1), 0) <= 1 - z[i, j, k], name=f'col_pre_{i}_{j}_{k}_{ip}')
 # set the objective function
